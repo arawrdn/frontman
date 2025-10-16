fe93a06e55620087b2e5f17dd3e239300ae71795
Itay Adler <itayadler@gmail.com>
2025-10-16 11:27:33 +0300

    integrate middleware with client

--- test/sites/blog-starter/src/middleware.ts
+++ test/sites/blog-starter/src/middleware.ts
@@ -1,1 +1,5 @@
-export { createMiddleware, config } from '@ask-the-llm/nextjs/src/Nextjs__Middleware.res.mjs';
+import { createMiddleware, config } from '@ask-the-llm/nextjs/src/Nextjs__Middleware.res.mjs';
+
+let middleware = createMiddleware(true)
+
+export { middleware, config }
