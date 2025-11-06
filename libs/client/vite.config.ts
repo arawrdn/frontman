daab059447e5e33c21692648d1614b61b6619832
Itay Adler <itayadler@gmail.com>
2025-11-06 22:56:25 +0200

    remove rescript plugin for client

--- libs/client/vite.config.ts
+++ libs/client/vite.config.ts
@@ -1,10 +1,9 @@
 import path from "node:path";
-import createReScriptPlugin from "@jihchi/vite-plugin-rescript";
 import tailwindcss from "@tailwindcss/vite";
 import * as vite from "vite";
 
 export default vite.defineConfig({
-	plugins: [createReScriptPlugin(), tailwindcss()],
+	plugins: [tailwindcss()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
